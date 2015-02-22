db2b41662040c5fad6c6ff4be857fb9f020d74b4
mode777 <mode7@gmx.net>
2015-02-22 14:13:12 -0700

    Changed files

--- LuaJitServer/conf.lua
+++
@@ -1,6 +0,0 @@
-return {
-    staticFilePath = "E:/users/Alex/Dropbox/code/Web/LuaJitServer/static",
-    defaultPath = "/index.html",
-    cacheSize = 300000000, --300mb,
-    dataLocation = "E:/users/Alex/Dropbox/code/Web/LuaJitServer/data"
-}
--- LuaJitServer/server.lua
+++
@@ -1,6 +0,0 @@
-io.output():setvbuf("no")
-local http = require "http"
-local cs = require "coroutineSheduler"
-local server = http.createServer("*",80)
-server.run()
-cs.run()
