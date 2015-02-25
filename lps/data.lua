d87dc47cd4f8c6f1125de361a35356c5253b2839
mode777 <mode7@gmx.net>
2015-02-25 13:26:56 -0700

    a# Please enter the commit message for your changes. Lines starting

--- lps/data.lua
+++ lps/data.lua
@@ -1,4 +1,4 @@
 return {
-    directory = "E:/users/alex/dropbox/code/web/server/lps/data/"
-    --directory = "/root/luaserver/lps/data/"
+    --directory = "E:/users/alex/dropbox/code/web/server/lps/data/"
+    directory = "/root/luaserver/lps/data/"
 }
