4aef6b5079d30bb65904cfd8fc226d5e38d4d07a
mode777 <mode7@gmx.net>
2015-03-14 02:03:10 -0600

    x

--- conf.lua
+++ conf.lua
@@ -1,13 +1,13 @@
 return {
-    --staticFilePath = "/root/LuaJitServer/static",
-    staticFilePath = "E:/users/alex/dropbox/code/web/server/static",
-    serverRoot = "E:/users/alex/dropbox/code/web/server",
+    staticFilePath = "/root/luaserver/static",
+    --staticFilePath = "E:/users/alex/dropbox/code/web/server/static",
+    serverRoot = "/root/luaserver",
     --staticFilePath = "C:/users/alex/dropbox/code/web/server/static",
     defaultPath = "/index.html",
-    disableCaching = true,
+    disableCaching = false,
     cacheSize = 300000000, --300mb,
     --dataLocation = "/root/LuaJitServer/data"
-    dataLocation = "E:/users/alex/dropbox/code/web/server/data",
+    --dataLocation = "E:/users/alex/dropbox/code/web/server/data",
     --dataLocation = "C:/users/alex/dropbox/code/web/server/data"
     keepAliveTimeout = 300, --5min,
     appIdentity = ".lua"
