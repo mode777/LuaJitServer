952d5a47b1cd9a2db5ec4c8f21d674af6da32e62
mode777 <mode7@gmx.net>
2015-03-16 22:31:40 +0100

    Confiig

--- conf.lua
+++ conf.lua
@@ -4,7 +4,7 @@ return {
     --staticFilePath = "C:/users/alex/dropbox/code/web/server/static",
     serverRoot = "/root/luaserver",
     serverRoot = "E:/users/alex/dropbox/code/web/server",
-    --serverRoot = "C:/users/alex/dropbox/code/web/server",
+    serverRoot = "C:/users/alex/dropbox/code/web/server",
     disableCaching = true,
     cacheSize = 300000000, --300mb,
     keepAliveTimeout = 300, --5min,
